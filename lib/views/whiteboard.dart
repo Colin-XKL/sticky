fc73cbce919522ad17ada80b09c397a3d9c16125
Colin <Colin_XKL@outlook.com>
2021-04-23 00:04:26 +0800

    feat: add button bar for content card

--- lib/views/whiteboard.dart
+++ lib/views/whiteboard.dart
@@ -58,6 +58,22 @@ class _ResizableCardState extends State<ResizableCard> {
     });
   }
 
+  Widget _getFunctionButton(
+      IconData icon, String tooltip, void Function() onPressed) {
+    return IconButton(
+      icon: Icon(icon),
+      onPressed: onPressed,
+      padding: EdgeInsets.zero,
+      tooltip: tooltip,
+      splashRadius: 18,
+      color: Colors.grey[700],
+
+      // splashColor: Theme.of(context).accentColor,
+      // focusColor: Theme.of(context).accentColor,
+      // hoverColor: Theme.of(context).accentColor,
+    );
+  }
+
   Widget _getContentCard(double height, double width, Widget child) {
     return Container(
         height: height,
@@ -75,26 +91,64 @@ class _ResizableCardState extends State<ResizableCard> {
                               color: Colors.grey.shade200,
                               width: 1,
                               style: BorderStyle.solid))),
-                  child: ListTile(
-                    title: Text(
-                      "Text",
-                      style: TextStyle(fontWeight: FontWeight.w600),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Tooltip(
+                              message: "Drag to move the card",
+                              child: ManipulatingBall(
+                                child: Icon(Icons.text_fields),
+                                onDrag: (dx, dy) {
+                                  setState(() {
+                                    widget.top = (widget.top + dy) + .0;
+                                    widget.left = (widget.left + dx) + .0;
+                                    widget.top =
+                                        widget.top > 0 ? widget.top : 0;
+                                    widget.left =
+                                        widget.left > 0 ? widget.left : 0;
+                                  });
+                                },
+                                dragAreaLength: 48,
+                              )),
+                        ),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: Text(
+                              "Text",
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          child: Row(
+                            children: [
+                              ButtonBar(
+                                buttonPadding: EdgeInsets.zero,
+                                children: [
+                                  _getFunctionButton(
+                                      Icons.arrow_forward_ios, "asd", () {}),
+                                  _getFunctionButton(
+                                      Icons.add_a_photo_rounded, "asd", () {}),
+                                  _getFunctionButton(
+                                      Icons.airline_seat_recline_normal_sharp,
+                                      "asd",
+                                      () {}),
+                                  _getFunctionButton(
+                                      Icons.assistant_sharp, "asd", () {}),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                      // dense: true,
                     ),
-                    leading: Tooltip(
-                        message: "Drag to move the card",
-                        child: ManipulatingBall(
-                          child: Icon(Icons.text_fields),
-                          onDrag: (dx, dy) {
-                            setState(() {
-                              widget.top = (widget.top + dy) + .0;
-                              widget.left = (widget.left + dx) + .0;
-                              widget.top = widget.top > 0 ? widget.top : 0;
-                              widget.left = widget.left > 0 ? widget.left : 0;
-                            });
-                          },
-                          dragAreaLength: 48,
-                        )),
-                    dense: true,
                   ),
                 ),
                 Expanded(
