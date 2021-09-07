83b18889542f945ae0f21a7d7d4c810d069514e0
Colin <Colin_XKL@Outlook.com>
2021-09-07 23:35:46 +0800

    better settings page UI

--- lib/pages/setting.dart
+++ lib/pages/setting.dart
@@ -30,65 +30,83 @@ class SettingsPage extends StatelessWidget {
         ),
         body: SingleChildScrollView(
             child: Wrap(children: [
-              // Padding(
-              // padding: EdgeInsets.fromLTRB(24, 16, 24, 8),
-              // child:
-              ConstrainedBox(
-                  constraints: BoxConstraints(maxWidth: 600),
-                  child: Wrap(
-                    children: [
-                      Card(
-                        margin: EdgeInsets.all(16),
-                        child: Column(
-                          children: [
-                            Text(
-                                "WebDAV settings ( Default Provider: NutStore )"),
-                            Divider(),
-                            TextField(
-                              controller: _accountCtl,
-                              onEditingComplete: _saveAccount,
-                              decoration: InputDecoration(
-                                  hintText: "用户名或邮箱",
-                                  // border: InputBorder.none,
-                                  // focusedBorder: UnderlineInputBorder(),
-                                  enabledBorder: InputBorder.none),
-                            ),
-                            TextField(
-                              controller: _tokenCtl,
-                              onEditingComplete: _saveToken,
-                              decoration: InputDecoration(
-                                  hintText: "WebDav密钥",
-                                  // border: InputBorder.none,
-                                  // focusedBorder: UnderlineInputBorder(),
-                                  enabledBorder: InputBorder.none),
-                            ),
-                          ],
+          // Padding(
+          // padding: EdgeInsets.fromLTRB(24, 16, 24, 8),
+          // child:
+          ConstrainedBox(
+              constraints: BoxConstraints(maxWidth: 600),
+              child: Wrap(
+                children: [
+                  Card(
+                    margin: EdgeInsets.all(16),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(0, 12, 0, 2),
+                          child: Text(
+                              "WebDAV settings ( Default Provider: NutStore )",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                              )),
                         ),
-                      ),
-                      Card(
-                        margin: EdgeInsets.all(16),
-                        child: Column(
-                          children: [
-                            Text("The Settings"),
-                            Divider(),
-                            SwitchListTile.adaptive(
-                                title: Text("Dark Theme"),
-                                value: Get.isDarkMode,
-                                onChanged: (darkMode) {
-                                  // print("Clicked");
-                                  Get.changeTheme(Get.isDarkMode
-                                      ? ThemeData.light()
-                                      : ThemeData.dark());
-                                  ScaffoldMessenger.of(context).showSnackBar(
-                                      SnackBar(
-                                          content: Text("Switching theme...")));
-                                })
-                          ],
+                        Divider(),
+                        TextField(
+                          controller: _accountCtl,
+                          onEditingComplete: _saveAccount,
+                          decoration: InputDecoration(
+                              hintText: "用户名或邮箱",
+                              // border: InputBorder.none,
+                              // focusedBorder: UnderlineInputBorder(),
+                              prefixIcon: Icon(
+                                Icons.account_box_rounded,
+                                size: 24,
+                              ),
+                              enabledBorder: InputBorder.none),
                         ),
-                      )
-                    ],
-                  )),
-              // ),
-            ])));
+                        TextField(
+                          controller: _tokenCtl,
+                          onEditingComplete: _saveToken,
+                          decoration: InputDecoration(
+                              hintText: "WebDav密钥",
+                              // border: InputBorder.none,
+                              // focusedBorder: UnderlineInputBorder(),
+                              prefixIcon:
+                                  Icon(Icons.security_rounded, size: 22),
+                              enabledBorder: InputBorder.none),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Card(
+                    margin: EdgeInsets.all(16),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(0, 12, 0, 2),
+                          child: Text("The Settings",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                              )),
+                        ),
+                        Divider(),
+                        SwitchListTile.adaptive(
+                            title: Text("Dark Theme"),
+                            value: Get.isDarkMode,
+                            onChanged: (darkMode) {
+                              // print("Clicked");
+                              Get.changeTheme(Get.isDarkMode
+                                  ? ThemeData.light()
+                                  : ThemeData.dark());
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                      content: Text("Switching theme...")));
+                            })
+                      ],
+                    ),
+                  )
+                ],
+              )),
+          // ),
+        ])));
   }
 }
